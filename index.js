b7cda843aec9265ab3067a9a5c4439dedb87f023
dcrawsh <dcrawsh@gmail.com>
2020-05-13 21:34:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(array, fn) {
+    let newArray = [];
+    for(let i=0;i<array.length;i++){
+        newArray.push(fn(array[i]))
+    }
+    return newArray
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
