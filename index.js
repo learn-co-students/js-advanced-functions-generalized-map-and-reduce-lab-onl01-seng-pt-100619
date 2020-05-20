70350e948cdd8b87a2220d843436e3f60e87f41b
github username <github email address>
2020-05-20 19:49:55 -0400

    Done.

--- index.js
+++ index.js
@@ -6,17 +6,34 @@ const map = (sourceArray, func) => {
     return newArray
   }
   
-const reduce = (sourceArray, func, startingPoint) => {
-    let value, valTF
-    if (startingPoint) {
-      value = startingPoint
-      valTF = 0
-    } else {
-      value = sourceArray[0]
-      valTF = 1
-    }
-    for (let i = valTF; i < sourceArray.length; i++) {
-      value = func(value, sourceArray[i])
-    }
-    return value
-};
+// const reduce = (sourceArray, func, startingPoint) => {
+//     let value, valTF
+//     if (startingPoint) {
+//       value = startingPoint
+//       valTF = 0
+//     } else {
+//       value = sourceArray[0]
+//       valTF = 1
+//     }
+//     for (let i = valTF; i < sourceArray.length; i++) {
+//       value = func(value, sourceArray[i])
+//     }
+//     return value
+// };
+
+
+function reduce(sourceArray, func, startingPoint){
+  let total 
+  let i = 0
+
+  if (startingPoint) {
+    total = startingPoint
+  } else {
+    total = sourceArray[0]
+    i++
+  }
+  for (i; i < sourceArray.length; i++){
+    total = func(sourceArray[i], total)
+  }
+  return total
+}
