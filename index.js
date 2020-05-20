d2a0b2795d1ce32560c5dfe603e587ba55c90737
github username <github email address>
2020-05-19 22:33:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+const map = (sourceArray, func) => {
+    const newArray = []
+    for (const x of sourceArray) {
+      newArray.push(func(x))
+    }
+    return newArray
+  }
+  
+const reduce = (sourceArray, func, startingPoint) => {
+    let value, valTF
+    if (startingPoint) {
+      value = startingPoint
+      valTF = 0
+    } else {
+      value = sourceArray[0]
+      valTF = 1
+    }
+    for (let i = valTF; i < sourceArray.length; i++) {
+      value = func(value, sourceArray[i])
+    }
+    return value
+};
