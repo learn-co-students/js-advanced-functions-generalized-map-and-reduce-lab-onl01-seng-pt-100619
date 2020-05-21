7029cce199b12838e3c7986ee0764ff94b6a3ed6
steelerx155 <steelerx155@gmail.com>
2020-05-21 19:01:42 -0400

    Done.

--- .history/index_20200521185453.js
+++ .history/index_20200521185453.js
@@ -8,13 +8,11 @@ function map(sourceArray, cb){
     return negNum
 }
 
-function reduce(src, cb, starting){
-    let r = (!!starting) ? starting : src[0]
-    let i = (!!starting) ? 0 : 1
-  
-    for (; i < src.length; i++) {
-      r = cb(src[i], r)
-    }
-  
-    return r;
-  }
+function reduce(sourceArray, cb){
+    let negNum = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        let theResult = sourceArray[i]
+        negNum.push(cb(theResult))    
+}
+return negNum
+}
