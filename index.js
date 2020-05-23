3301bc79e698f9afa7a912e3ee7088bba5c85cf8
LizLaffitte <liz.kossakowski@gmail.com>
2020-05-23 19:51:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(sourceArray, cbFxn){
+    let newArray= []
+    for(let i = 0; i<sourceArray.length; i++){
+        newArray.push(cbFxn(sourceArray[i]))
+    }
+    return newArray
+}
+
+function reduce(sourceArray, cbFxn, startingPoint){
+    let runningTotal
+    let i
+    if(startingPoint){
+        runningTotal = startingPoint
+        i = 0
+    } else {
+        runningTotal = sourceArray[0]
+        i = 1
+    }
+    
+    for( i; i < sourceArray.length; i++){
+        runningTotal = cbFxn(sourceArray[i], runningTotal)
+    }
+    return runningTotal
+}
