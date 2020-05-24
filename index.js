77b0db93990aa82ff46e45f325342305ad60d03c
github username <github email address>
2020-05-24 11:28:57 -0400

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,22 @@
 function map(sourceArray, callbackFunction){
   const newArray = []
   for (let i = 0; i < sourceArray.length; i++){
-      newArray.push(callbackFunction(source[i]))
+      newArray.push(callbackFunction(sourceArray[i]))
   }
   return newArray
+}
+
+function reduce(sourceArray, callbackFunction, startingPoint){
+  let total
+  let i = 0
+  if (startingPoint){
+    total = startingPoint
+  } else {
+    total = sourceArray[0]
+    i++
+  }
+  for (i; i < sourceArray.length; i++){
+    total = callbackFunction(sourceArray[i], total)
+  }
+  return total
 }
